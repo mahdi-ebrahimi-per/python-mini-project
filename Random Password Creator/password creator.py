bcbab6b567f3a83ec737b507be6ec93574cc93d0
mahdi-ebrahimi-per <76206312+mahdi-ebrahimi-per@users.noreply.github.com>
2021-07-03 14:28:24 +0430

    Update password creator.py

--- Random Password Creator/password creator.py
+++ Random Password Creator/password creator.py
@@ -1,7 +1,7 @@
 import string
 import random 
 
-from numpy.random import rand
+
 char = string.ascii_letters + string.punctuation + string.digits
 password_length = range(random.randint(8, 16))  # random length
 password = "".join(random.choice(char)for x in password_length)
