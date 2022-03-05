c81e4839565c501b301a225b32d0c6a5ed053a30
mahdi-ebrahimi-per <76206312+mahdi-ebrahimi-per@users.noreply.github.com>
2022-03-05 10:18:50 +0330

    Create spammer.py

---
+++ Chat spammer/spammer.py
@@ -0,0 +1,33 @@
+import pyautogui
+import time
+
+# time.sleep(3)
+
+# for i in range(10):
+#     pyautogui.typewrite("++++++++++++")
+#     pyautogui.press("enter")
+    
+
+
+
+time.sleep(1)
+
+for i in range(5):
+    pyautogui.typewrite("\U0000062C")
+    pyautogui.press("enter")
+
+
+
+#_____________
+
+# with open("pssword list.txt", 'r') as File:
+#     # print(type(File.readlines()
+#     # 
+
+
+#     for i in list(File.readlines()):
+#         i = i.split() 
+#         print(i[0])
+#         pyautogui.typewrite(str(i))
+#         pyautogui.press("enter")
+#         time.sleep(5)
